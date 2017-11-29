8c24ed81fc5821185fa9b31fed2a6670cfc0c5b0
falah <shaikh_falah@hotmail.com>
2017-11-29 08:38:42 +0530

    guess the number game

--- first.py
+++ first.py
@@ -1,9 +1,29 @@
-print "Testing 123"
+number =23
+running = True
+current_points=0
+tries_remaining=100;
 
-i=5
-i=i+1
-print i
-s = '''This is a multi - line string.
+while running:
+    guess=int(raw_input("enter a number to guess"))
+    if guess==number:
+        print "Congratulations You have guessed it right "
+        print "your Score is now increased to "+current_points++
+        number=Math.rand(1,7000)
+        tries_remaining--
+        c=raw_input("do u want to continue playing /n 1--yes /n 2--no")
+        if c==1:
+            continue
+        else:
+            break;
 
-this is the seconde line '''
-print s
+
+    else if guess<number:
+        print "the secret number is more then what u have guessed"
+        tries_remaining--
+        print tries remaining + tries_remaining
+
+
+    else if guess>number:
+        print "the secret number is less then what u have guessed"
+        tries_remaining--
+        print tries remaining + tries_remaining
